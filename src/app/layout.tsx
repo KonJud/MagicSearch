5e7c45b7fc86ed3a0548a33498b180e18361b3ba
KonJud <deveuudes@gmail.com>
2024-07-17 02:22:12 +0000

    search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Icons } from "@/components/Icons";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +51,16 @@ export default function RootLayout({
               <h1 className="tracking-tight text-4xl sm:text-6xl font-bold">
                 MagicSearch 
               </h1>
+
+              <p className="max-w-xl text-center text-lg text-slate-700">
+                A beautifully designed, hybrid search engine that enhances search accuracy by querying semantically related results.
+              </p>
+
+              <div className="mx-auto mt-16 w-full max-w-2xl flex flex-col">
+                <SearchBar />
+
+                {children}
+              </div>
             </div>
           </div>
         </div>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import React, { useRef, useState, useTransition } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+const SearchBar = () => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    const [isSearching, startTransition] = useTransition()
+    const router = useRouter()
+    const [query, setQuery] = useState<string>("")
+
+    const search = () => {
+        startTransition(() => {
+            router.push(`/search?query=${query}`)
+        })
+    }
+
+  return (
+    <div className='relative w-full h-14 flex flex-col bg-white'>
+        <div className="relative h-14 z-10 rounded-md">
+            <Input 
+                disabled={isSearching}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={(e) => {
+                    if(e.key === "Enter") {
+                        search()
+                    }
+
+                    if (e.key === "Escape") {
+                        inputRef?.current?.blur()
+                    } 
+                }}
+                ref={inputRef}
+                className='absolute inset-0 h-full' 
+            />
+
+            <Button 
+                size={"sm"}
+                onClick={search}
+                className='absolute right-0 inset-y-0 h-full rounded-l-none'
+            >
+                <Search className='h-6 w-6'/>
+            </Button>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
