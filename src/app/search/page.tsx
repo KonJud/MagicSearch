97c7d85b3904c7903c6f98c82805b3f096687eeb
KonJud <deveuudes@gmail.com>
2024-07-17 10:30:45 +0000

    Config

---
+++ src/app/search/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+const Page = ({ searchParams }: PageProps) => {
+    const query = searchParams.query 
+
+    if (Array.isArray(query) || !query) {
+        return redirect("/")
+    }
+
+    //querying logic
+    
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@
 import React, { useRef, useState, useTransition } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
-import { Search } from 'lucide-react'
+import { Loader2, Search } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
 const SearchBar = () => {
@@ -39,11 +39,12 @@ const SearchBar = () => {
             />
 
             <Button 
+                disabled={isSearching}
                 size={"sm"}
                 onClick={search}
                 className='absolute right-0 inset-y-0 h-full rounded-l-none'
             >
-                <Search className='h-6 w-6'/>
+                {isSearching ? <Loader2 className='h-6 w-6 animate-spin' /> : <Search className='h-6 w-6'/>}
             </Button>
         </div>
     </div>
